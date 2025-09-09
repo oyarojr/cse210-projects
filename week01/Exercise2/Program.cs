7fbc5839cc3227fc1e55ec8a436de2efcde0e468
oyarojr <oyarodan2024@outlook.com>
2025-09-09 08:37:44 +0300

    exercise 1-3 completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,96 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Key in your Grade Percentage: ");
+        string userScore = Console.ReadLine();
+        int x = int.Parse(userScore);
+
+        string letter = null;
+
+        if (x >= 90)
+        {
+            if (x < 93)
+            {
+                letter = "A-";
+            }
+            else if (x < 97)
+            {
+                letter = "A";
+            }
+            else if (x >= 97)
+            {
+                letter = "A+";
+            }
+            // Console.WriteLine($"Your Grade is: {myString}");
+        }
+        else if (x >= 80)
+        {
+            if (x < 83)
+            {
+                letter = "B-";
+            }
+            else if (x < 87)
+            {
+                letter = "B";
+            }
+            else if (x >= 87)
+            {
+                letter = "B+";
+            }
+            // letter = "B";
+            // Console.WriteLine($"Your Grade is: {myString}");
+        }
+        else if (x >= 70)
+        {
+            if (x < 73)
+            {
+                letter = "C-";
+            }
+            else if (x < 77)
+            {
+                letter = "C";
+            }
+            else if (x >= 77)
+            {
+                letter = "C+";
+            }
+        }
+        else if (x >= 60)
+        {
+            if (x < 63)
+            {
+                letter = "D-";
+            }
+            else if (x < 67)
+            {
+                letter = "D";
+            }
+            else if (x >= 67)
+            {
+                letter = "D+";
+            }
+        }
+        else if (x < 60)
+        {
+            letter = "F";
+            // Console.WriteLine($"Your Grade is: {myString}");
+        }
+        else
+        {
+            Console.WriteLine("Your will have to redo the class");
+        }
+
+        string message = null;
+        if (x >= 70)
+        {
+            message = "Congratulations!You have passed the exams";
+        }
+        else
+        {
+            message = "Oops! You have to retake the class";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        Console.WriteLine(message);
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        // Console.Write("What is the Magic Number? ");
+        // string magicNumber = Console.ReadLine();
+        // int number = int.Parse(magicNumber);
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+
+        string guess = "100000000";
+        int guessCln = int.Parse(guess);
+
+        do
+        {
+            Console.WriteLine("What is your Guess: ");
+            guess = Console.ReadLine();
+            guessCln = int.Parse(guess);
+
+
+            if (guessCln > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessCln < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessCln == number)
+            {
+                Console.WriteLine("You guessed it right!");
+            }
+        } while (guessCln != number);
     }
 }
