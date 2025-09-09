6de27a53ed65b5b7aef841215d7c4deb6e3176c8
oyarojr <oyarodan2024@outlook.com>
2025-09-09 12:10:40 +0300

    Completed Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        string indNum = "1000000";
+        int indNumCln = int.Parse(indNum);
+
+        string sumNums = "0";
+        int sumNumsCln = int.Parse(sumNums);
+
+        string sumInts = "0";
+        int sumIntsCln = int.Parse(sumInts);
+
+
+        do
+        {
+            Console.WriteLine("Key in a number to add to the list: ");
+            indNum = Console.ReadLine();
+            indNumCln = int.Parse(indNum);
+
+            if (indNumCln != 0)
+            {
+                numbers.Add(indNumCln);
+                sumNumsCln = sumNumsCln + indNumCln;
+                sumIntsCln = sumIntsCln + 1;
+            }
+
+        } while (indNumCln != 0);
+
+        Console.WriteLine($"The sum is: {sumNumsCln}");
+        int avg = sumNumsCln / sumIntsCln;
+        Console.WriteLine($"The average is: {avg}");
+
+        int highest = -5;
+        int lowest = 10000000;
+
+        foreach (int sth in numbers)
+        {
+            if (sth > highest)
+            {
+                highest = sth;
+            }
+        }
+
+        foreach (int sth in numbers)
+        {
+            if (sth < lowest)
+            {
+                lowest = sth;
+            }
+        }
+        Console.WriteLine($"The largest number is: {highest}");
+        Console.WriteLine($"The smallest positive number is: {lowest}");
+
+
+
     }
+
 }
