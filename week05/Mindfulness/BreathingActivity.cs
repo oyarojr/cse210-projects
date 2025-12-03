770d4afc65f1d485b984b674c05811f852be51fd
oyarojr <oyarodan2024@outlook.com>
2025-12-03 10:12:08 +0300

    Working on the Breathing Activity Class

---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,6 @@
+   class BreathingActivity : Activity
+    {
+        public BreathingActivity()
+            : base("Breathing", "This activity will help you relax by guiding you to breathe in and out slowly.") { }
+
+    }
