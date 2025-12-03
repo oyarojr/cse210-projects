fb2b739420c2e7ffb03d4fcfd852660ae3d1be4a
oyarojr <oyarodan2024@outlook.com>
2025-12-03 14:32:50 +0300

    Finished on the breathingactivity class

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,6 +1,24 @@
-   class BreathingActivity : Activity
-    {
-        public BreathingActivity()
-            : base("Breathing", "This activity will help you relax by guiding you to breathe in and out slowly.") { }
+class BreathingActivity : Activity
+{
+    public BreathingActivity()
+        : base("Breathing", "This activity will help you relax by guiding you to breathe in and out slowly.") { }
 
+    public void Run()
+    {
+        DisplayStartingMessage();
+        int elapsed = 0;
+        while (elapsed < Duration)
+        {
+            Console.WriteLine("\nBreathe in...");
+            ShowCountDown(3);
+            elapsed += 3;
+
+            if (elapsed >= Duration) break;
+
+            Console.WriteLine("Breathe out...");
+            ShowCountDown(3);
+            elapsed += 3;
+        }
+        DisplayEndingMessage();
     }
+}
