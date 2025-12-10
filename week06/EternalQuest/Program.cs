a3c4f94347ae35b6572dec73946d7d9a6b8e93cd
oyarojr <oyarodan2024@outlook.com>
2025-12-10 21:52:43 +0300

    Program class done

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -6,15 +6,12 @@ namespace EternalQuestApp
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("Eternal Quest - Gamified Goal Tracker");
-            // Console.WriteLine("-------------------------------------");
-            // var manager = new GoalManager();
-            // manager.Start();
-            // Console.WriteLine("Goodbye!");
-            SimpleGoal goal = new SimpleGoal("Read Book", "Finish reading 'The Great Gatsby'", "50");
-            goal.RecordEvent();
-            Console.WriteLine(goal.GetDetails());
-            
+            Console.WriteLine("Eternal Quest - Gamified Goal Tracker");
+            Console.WriteLine("-------------------------------------");
+            var manager = new GoalManager();
+            manager.Start();
+            Console.WriteLine("Goodbye!");
+
 
         }
     }
