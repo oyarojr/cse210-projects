439813cc3fdb27da532b64fd3399bbbbc1753ee1
oyarojr <oyarodan2024@outlook.com>
2025-12-11 21:12:41 +0300

    programming with classes docs and the activity class for week 7

---
+++ week07/Exercise.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+abstract class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public DateTime Date => _date;
+    public int Minutes => _minutes;
+
+    public abstract double GetDistance(); 
+    public abstract double GetSpeed();    
+    public abstract double GetPace();     
+
+    public virtual string GetSummary()
+    {
+        return $"{Date:dd MMM yyyy} {GetType().Name} ({Minutes} min): " +
+               $"Distance {GetDistance():0.0} km, " +
+               $"Speed {GetSpeed():0.0} kph, " +
+               $"Pace {GetPace():0.00} min per km";
+    }
+}
